6a011f5935ebf46a6cec3a66ccd474b37e4bbfa2
llDeRuNll <eduardprudiys@gmail.com>
2025-05-25 01:41:54 -0400

    basic homePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,16 @@
-const HomePage = () => {
-  return <div>HomePage</div>;
-};
+import { Link } from "react-router-dom";
+import s from "./HomePage.module.css";
+
+const HomePage = () => (
+  <div className={s.homePageContainer}>
+    <div className={s.homepagetextWrapper}>
+      <h1>Find your perfect rental car</h1>
+      <p>Reliable and budget-friendly rentals for any journey</p>
+      <Link to="/catalog" className={s.ctaButton}>
+        View Catalog
+      </Link>
+    </div>
+  </div>
+);
 
 export default HomePage;
